cf9b099329a6f7fad1aadd7ed12f87861eac5bef
Joel <j.n.brewster@gmail.com>
2016-05-19 11:52:57 +1000

    working version

--- ruby_101/omdb/app.rb
+++ ruby_101/omdb/app.rb
@@ -1,24 +1,25 @@
 require 'httparty'
 require 'pp'
-# require 'sinatra'
-# require 'sinatra/reloader'
-
-# @title = "Home"
-# @omdb_data = HTTParty.get "http://www.omdbapi.com/?t=aliens&y=&plot=short&r=json"
+require 'sinatra'
+require 'sinatra/reloader'
 
 api_url = "http://www.omdbapi.com/?"
 
 # response = HTTParty.get "#{api_url}t=Harry Potter"
 # response = HTTParty.get "#{api_url}t=Lord of the rings"
-response = HTTParty.get "#{api_url}t=Alien&plot=full"
-pp response
+# response = HTTParty.get "#{api_url}t=Alien&plot=full&tomatoes=true"
+# pp response
 
 # # Home route
-# get '/' do
-#   "Index page"
-#   # Insert json stuff here
-#   # http://www.omdbapi.com/?
-# end
+get '/' do
+  erb :index
+end
+
+post '/search' do
+  params[:searchterm]
+  @result = HTTParty.get "#{api_url}s=#{params[:searchterm]}"
+  erb :search
+end
 
 # # About route
 # get '/about' do
