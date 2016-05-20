b59fb2b1a68bed898923f85b0cba5daf4c4dcc9f
Joel <j.n.brewster@gmail.com>
2016-05-20 16:08:41 +1000

    done

--- ruby_101/starbucks.rb
+++ ruby_101/starbucks.rb
@@ -8,36 +8,33 @@ class Coffee
     @type = type
     @sugar = sugar
     @size = size
-    @name = name
+    # => "Barrel's latte, medium, 2 sugars."
+    @name = name.gsub(/[aei]/, {"a" => "e", "e" => "i", "i" => "q" })
+    @shot = shot
+    @created_at = Time.now
+    @brewing_time = Random.rand(120..300)
   end
   # Write the .to_s method for this class so when you puts c1 you will have "Darryl's latte, medium, 2 sugars."
   def to_s
-    return "#{@name}'s #{@type}, #{@size}, #{@sugar} sugars."
+    "#{@name}\'s #{@type}, #{@size}, #{@sugar} sugars."
   end
   # When you make a new order, you have to wait for coffee. When you create the new order, there should be a waiting time of anywhere between 2-5 minutes. You should be able to check if it's ready by calling c1.ready? and have the program yell out the order and the misspelled name.
-  def ready
-    @current_time = current_time
-    @wait_time = wait_time
-    current_time = Time.new
-    wait_time = current_time.min + 2
-    collected = false
-    if current_time = wait_time
-      return to_s
-      # You should be able to mark the coffee as collected with c1.collected = true.
-      collected = true
+  def ready?
+    if Time.now > (@created_at + @brewing_time)
+      "ORDER UP #{@name}.upcase"
+    else
+      time_left = (@created_at + @brewing_time) - Time.now
+      "WAIT YOUR TURN! #{time_left.divmod(60)[0]}:#{time_left.divmod[60][1].round} SECONDS LEFT!"
     end
-
   end
 end
 
 # Starbucks always gets names wrong, however. Make the name a bad misspelling of the actual name when you create the order.
 # puts c1
-# # => "Barrel's latte, medium, 2 sugars."
 
 c1 = Coffee.new('latte','2','medium','Darryl')
-c1 = Coffee.new('latte','2','medium','Barryl')
 puts c1
-# puts c1.collected?
+puts c1.ready?
 
 
 # List all of the coffees not collected.
